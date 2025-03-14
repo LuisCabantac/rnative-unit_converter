c1cfdd378a1f6e96d2bc68e226bf79e869ab956a
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-03-15 05:48:52 +0800

    Add a background color to the root app

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,23 @@
 import { Stack } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+import { Colors } from "@/utils/Colors";
 
 export default function Index() {
   return (
     <>
       <Stack.Screen options={{ headerShown: false }} />
-      <Text>Unit Converter</Text>
-    </View>
+      <View style={styles.container}>
+        <Text>Unit Converter</Text>
+      </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: Colors.backgroundColor,
+  },
+});
