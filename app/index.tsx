78e8da6a253f17d4fce5ac1c1fc64d0467050586
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-03-15 14:14:53 +0800

    feat: Add custom fonts to the project

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,14 @@
 import { Stack } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
 
 import { Colors } from "@/utils/Colors";
 
 export default function Index() {
+  const [loaded] = useFonts({
+    "Outfit-Regular": require("../assets/fonts/Outfit-Regular.ttf"),
+    "Outfit-Bold": require("../assets/fonts/Outfit-Bold.ttf"),
+  });
+
   return (
     <>
       <Stack.Screen options={{ headerShown: false }} />
